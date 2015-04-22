57319782bf02823452e23640939b4395c6f8bb82
Ted Wen <ted@makeitsocial.com>
2015-04-22 17:30:42 +0100

    implemented partial

--- README.md
+++ README.md
@@ -10,31 +10,93 @@ See https://gapipay.appspot.com/developer
 
 ## Set up
 
-In the server.js or app.js file, add the following.
+In the server.js or app.js file, import the module with a client username and password.
 
 ```js
-var mispay = require('mispay');
-
-mispay.config({
-	username: '...',
-	password: '...'
-});
+var mispay = require('mispay')('username','password');
 ``` 
+
+## Use express router
+
+mispay.js uses the hard-coded route path '/mispay'. Use a bodyParser to parse JSON body content.
+
+```js
+var express = require('express');
+var router = express();
+
+mispay.route(router);
+```
 
 ## Front-end
 
-Reference the mpay.js or mpay.min.js in the html page as front end. Configure static path in express:
+To allow referecing the scripts, configure a static directory on express:
 
 ```js
-server.use('/mispay', express.static(mispay.staticPath));
+router.use('/mispay/js', express.static(mispay.staticPath));
 ```
 
-In the browser:
+Reference the mispay-ng.js, the AngularJs-based script, in the html page:
+
 ```html
-<script src="/mispay/js/mpay.min.js"></script>
+<script src="/mispay/js/mangopay-kit.min.js"></script>
+<script src="/mispay/js/mispay-ng.js"></script>
+```
+and 'ccs.js' for country codes if necessary.
+```html
+<script src="/mispay/js/css.js"></script>
 ```
 
+### The pay button
+This is a demo of doing a direct card payment.
+```html
+<button ng-click="pay()">Pay</button>
+```
+
+```js
+var basket = {
+	id: '1234', //optional
+	gid: '1', //optional
+	buyer: {
+		xid: '100' //local id submitted previously
+	},
+	items: [
+		{
+			sid: '12334',
+			item: 'product-1',
+			currency: 'GBP',
+			amount: 10000, //in cents
+		}
+	]
+};
+//in ngController:
+$scope.pay = function(){
+	MisPay.Request.post(basket, function(r){
+		if (r.id) {
+			$scope.requestId = r.id;
+			setTimeout(checkout, 200); //leave some time for the gapipay server
+		} else {
+			alert(r);
+		}
+	});
+};
+function checkout(){
+	MisPay.Request.checkout($scope.requestId, function(pay){
+		if (pay.pays && pay.pays.length > 0){
+			$scope.payment = pay; //show user the basket and let them pay one by one
+		} else if (pay.pays.length == 0){
+			setTimeout(checkout, 200); //poll until the request handled.
+		} else {
+			alert(pay);
+		}
+	});
+}
+```
+
+### Pre-authorize a card payment
 
 ## Developing
 
+To test, install mocha and chai
+	npm install -g mocha
+	npm install chai --save-dev
     npm test
--- test/mispaySpec.js
+++ test/mispaySpec.js
@@ -1,7 +1,93 @@
 var expect = require('chai').expect;
 var mispay = require('../lib/mispay.js')("user1","pass1");
 
-describe('mispay', function(){
+/**
+ * MockHttpServe({
+		returnData: function(data){..},
+		returnError: function(){},
+		valid: function(req, data){ return true; },
+	});
+ */
+function MockHttpServe(callbacks){
+	var _mock = this,
+		_req = {}, _data = {},
+		_callbacks = callbacks || {},
+		_res = {
+			on: function(op, cb){
+				_callbacks.response = cb;
+			},
+			setEncoding: function(enc){
+			},
+		};
+	this.callbacks = function(callbacks){
+		_callbacks = callbacks;
+	};
+	this.request = function(option, cb){
+		//console.log(option);
+		_req = option;
+		cb(_res);
+		return _mock;
+	}
+	this.write = function(data){
+		_data = data;
+	};
+	this.end = function(){
+		var v = true;
+		if (_callbacks.valid) {
+			v = _callbacks.valid(_req, _res.data);
+		}
+		if (!v) {
+			var e=_callbacks.returnError ? _callbacks.returnError() : 'error';
+			_callbacks.onError(e);
+		} else {
+			var data = _callbacks.returnData ? _callbacks.returnData(_data) : '{}';
+			_callbacks.response(data);
+		}
+	};
+	this.on = function(op, cb){
+		if (op=='error' && cb){
+			_callbacks.onError = cb;
+		}
+	};
+}
+
+describe('mispay http', function(){
+	var mock1;
+	beforeEach(function(done){
+		mock1 = new MockHttpServe();
+		mispay.config({http:mock1});
+		done();
+	});
+	describe('get',function(){
+		it('get-1',function(done){
+			mock1.callbacks({
+				valid: function(req,data){
+					expect(req.method).to.equal('GET');
+					expect(req.path).to.equal('/v1/request/1234/checkout');
+					expect(req.hostname).to.equal('gapipay.appspot.com');
+					expect(req.auth).to.equal('user1:pass1');
+					expect(req.headers).to.have.property('accept','application/json');
+					expect(req.headers).to.have.property('Content-Type','application/json');
+					return true;
+				}
+			});
+			mispay.checkout('1234', function(r){
+				expect(r).to.exist;
+				done();
+			}, function(e){
+				console.log('error:',e);
+				done();
+			});
+		});
+	});
+	describe('post',function(){
+		it('post-1',function(done){
+			done();
+		});
+	});
+});
+
+describe('mispay object', function(){
 	describe('config', function(){
 		it('should set user and pass', function(){
 			expect(mispay.cred().username).to.equal('user1');
@@ -18,16 +104,16 @@ describe('mispay', function(){
 			expect(mispay.staticPath).to.contain('/lib/public');
 		});
 	});
-	describe('get method', function(){
-		it('should get from https', function(done){
-			mispay.get('agents', function(r){
-				//console.log('response:',r);
-				expect(r).to.equal('{"error":"Unauthorized"}');
-				done();
-			}, function(e){
-				console.log('error:',e);
-				done();
-			});
-		});
-	});
+//	describe('get method', function(){
+//		it('should get from https', function(done){
+//			mispay.get('agents', function(r){
+//				//console.log('response:',r);
+//				expect(r.error).to.equal("Unauthorized");
+//				done();
+//			}, function(e){
+//				console.log('error:',e);
+//				done();
+//			});
+//		});
+//	});
 });
